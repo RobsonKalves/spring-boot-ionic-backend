cf67632a4a56d2b0133caae6cd78396076cc9686
unknown <robson2012nascimento@hotmail.com>
2021-08-01 12:40:42 -0300

    Estado e Cidade

---
+++ src/main/java/com/robsonalves/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.robsonalves.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.robsonalves.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
