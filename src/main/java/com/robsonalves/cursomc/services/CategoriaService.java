0e7006ae016e051f831f9410736d56a763c911c9
unknown <robson2012nascimento@hotmail.com>
2021-08-03 23:30:28 -0300

    Listando todas categorias com DTO

--- src/main/java/com/robsonalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/robsonalves/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.robsonalves.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -50,6 +51,12 @@ public class CategoriaService {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
 		}
 		
+	}
+	
+	
+	public List<Categoria> findAll(){
+		
+		return repo.findAll();
 	}
 
 }
