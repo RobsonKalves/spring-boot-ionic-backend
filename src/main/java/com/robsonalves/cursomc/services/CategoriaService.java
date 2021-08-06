87f77213994871d661465fb78b6a30462254a735
unknown <robson2012nascimento@hotmail.com>
2021-08-05 22:51:27 -0300

    Correções: update em CategoriaService e construtor de Pagamento

--- src/main/java/com/robsonalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/robsonalves/cursomc/services/CategoriaService.java
@@ -31,48 +31,52 @@ public class CategoriaService {
 	}
 
 	public Categoria insert(Categoria obj) {
-		
+
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
-		
-		find(obj.getId()); //verifica se realmente existe um id para atualizar..
-		return repo.save(obj);
+
+		Categoria newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
 	}
-	
+
 	public void delete(Integer id) {
-		
+
 		find(id);
-		
+
 		try {
-			
+
 			repo.deleteById(id);
-			
+
 		} catch (DataIntegrityViolationException e) {
-			
+
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos!");
 		}
-		
+
 	}
-	
-	
-	public List<Categoria> findAll(){
-		
+
+	public List<Categoria> findAll() {
+
 		return repo.findAll();
 	}
-	
+
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
-		
+
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
-		
+
 		return repo.findAll(pageRequest);
 	}
-	
+
 	public Categoria fromDTO(CategoriaDTO objDTO) {
-		
+
 		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 
+	private void updateData(Categoria newObj, Categoria obj) {
+
+		newObj.setNome(obj.getNome());
+	}
 }
